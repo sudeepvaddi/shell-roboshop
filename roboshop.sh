a1dbf3542c6f2258933efbc7477a0302d1785a1c
sudeep <sudeepvaddi@gmail.com>
2025-06-01 12:19:23 +0530

    hello world started

---
+++ roboshop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-03724a8dfd2a68383"
+INSTANCES=("mongodb" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "disaptch" "frontend")
+ZONE_ID="Z0417974VNU4EJ9PF5SZ"
+DOMAIN_NAME="sudeepdevops.fun"
+
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03724a8dfd2a68383 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
